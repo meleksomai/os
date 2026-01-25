1b36a5275d407caceffbf02bd8dd791e9d72b315
Melek Somai <melek@rethinkhealth.io>
2026-01-25 18:09:28 -0500

    update template

--- packages/flags/endpoint.ts
+++ packages/flags/endpoint.ts
@@ -1,7 +1,8 @@
 import { createFlagsDiscoveryEndpoint, getProviderData } from "flags/next";
-import type { NextRequest } from "next/server";
+
 // biome-ignore lint/performance/noNamespaceImport: needed for dynamic flag imports
 import * as flags from "./index";
 
-export const getFlagsEndpoint: (request: NextRequest) => Promise<Response> =
+// biome-ignore lint/suspicious/noExplicitAny: needed for dynamic flag imports
+export const getFlagsEndpoint: (request: any) => Promise<Response> =
   createFlagsDiscoveryEndpoint(() => getProviderData(flags));
