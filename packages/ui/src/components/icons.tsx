76f5549e915d30650a421872cb4a552cf7b380c5
Melek Somai <melek@rethinkhealth.io>
2026-06-19 11:30:31 -0400

    fixed

--- packages/ui/src/components/icons.tsx
+++ packages/ui/src/components/icons.tsx
@@ -54,6 +54,15 @@ export function GitHubIcon(props: React.ComponentProps<"svg">) {
   );
 }
 
+export function XIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg viewBox="0 0 300 271" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <title>X</title>
+      <path d="m236 0h46l-101 115 118 156h-92.6l-72.5-94.8-83 94.8h-46l107-123-113-148h94.9l65.5 86.6zm-16.1 244h25.5l-165-218h-27.4z" />
+    </svg>
+  );
+}
+
 export const InfoIcon = (props: IconProps) => (
   <BaseIcon icon={InformationSquareIcon} {...props} />
 );
